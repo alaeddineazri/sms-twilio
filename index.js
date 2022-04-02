5b854be23ce93720eb842079369c78e6bc4493f5
azri <alaeddinazri@gmail.com>
2022-04-02 23:46:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use("/sms", require("./routes/sms"));
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
